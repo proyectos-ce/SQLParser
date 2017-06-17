fedf5a9bcae95985bd200a2221480b798160daaa
Joseph Vargas <joseph@cpokemon.com>
2017-06-16 22:40:24 -0600

    Avance CREATE TABLE

---
+++ UpdateDescriptor.h
@@ -0,0 +1,21 @@
+//
+// Created by joseph on 16/06/17.
+//
+
+#ifndef PARSER_UPDATEDESCRIPTOR_H
+#define PARSER_UPDATEDESCRIPTOR_H
+
+#include <iostream>
+
+class UpdateDescriptor {
+
+public:
+	std::string column;
+	std::string value;
+
+	UpdateDescriptor() {
+
+	}
+};
+
+#endif //PARSER_UPDATEDESCRIPTOR_H
--- main.cpp
+++ main.cpp
@@ -3,15 +3,22 @@
 #include <boost/regex.hpp>
 #include <regex>
 #include "WhereSelector.h"
+#include "UpdateDescriptor.h"
 
 void preprocess(std::string sql);
 
 void processSelect(std::basic_string<char, std::char_traits<char>, std::allocator<char>> basic_string);
 
+void processUpdate(std::string basic_string);
+
+std::vector<WhereSelector> getWheres(std::string wheres, std::string defaultTable);
+
+void processCreateTable(std::string basic_string);
+
 char whiteSpaces[] = " \t\r\n";
 
 int main() {
-	std::string sql = "SELECT data, numero,cedula FRoM ESTUDIANTES "
+	std::string sql = "  	\nSELECT data, numero,cedula FRoM ESTUDIANTES "
 			""
 			"\n"
 			"\n"
@@ -19,7 +26,15 @@ int main() {
 			""
 			"JOIN CURSOS \n ON ESTUDIANTES.ID = \nCURSOS.IDEST";
 
-	preprocess(sql);
+	std::string sql2 = "UPDATE table_name\n"
+			"SET col1 = value1, column2 = value2\n"
+			"WHERE condition < 4 AND COND2 = 5;";
+
+	std::string sqlCreate = "CREATE TABLE MATRICULA(IDCURSO INT REFERENCE(CURSO.ID), IDALUMNO INT\n"
+			"\n"
+			"REFERENCE(ESTUDIANTES.ID), NOMBRE STRING(10), PRIMARY KEY IDCURSO)";
+
+	preprocess(sqlCreate);
 
 	return 0;
 }
@@ -27,8 +42,9 @@ int main() {
 void preprocess(std::string sql) {
 	// Remove newlines and tabs, replace them with spaces
 	boost::replace_all(sql, "\r\n", " ");
-	boost::replace_all(sql, "\n", "");
+	boost::replace_all(sql, "\n", " ");
 	boost::replace_all(sql, "\t", " ");
+	boost::replace_all(sql, ";", "");
 	boost::trim(sql);
 
 	if (boost::empty(sql)) {
@@ -40,13 +56,31 @@ void preprocess(std::string sql) {
 	boost::to_lower(firstWord);
 	boost::trim(firstWord);
 
+	std::string presql = sql.substr(sql.find_first_not_of(whiteSpaces, sql.find_first_of(whiteSpaces,
+																						 sql.find_first_not_of(
+																								 whiteSpaces))));
+
 	if (firstWord == "select") {
-		processSelect(sql.substr(sql.find_first_not_of(whiteSpaces, sql.find_first_of(whiteSpaces,
-																					  sql.find_first_not_of(
-																							  whiteSpaces)))));
+		processSelect(presql);
+	} else if (firstWord == "update") {
+		processUpdate(presql);
+	} else if (firstWord == "create") {
+		std::string secondWord = presql.substr(0, presql.find(" "));
+		boost::to_lower(secondWord);
+		boost::trim(secondWord);
+		presql = presql.substr(presql.find(" "));
+		boost::trim(presql);
+		if (secondWord == "table") {
+			processCreateTable(presql);
+		}
+	} else {
+		std::cerr << "ERROR: Invalid SQL operation" << std::endl;
+		throw std::exception();
 	}
 
 }
+
+
 
 void processSelect(std::string sql) {
 	std::cout << "VOY A HACER SELECT CON:" << std::endl << sql << std::endl;
@@ -127,90 +161,9 @@ void processSelect(std::string sql) {
 			throw std::exception();
 		}
 
-		std::vector<std::string> wheres;
-		std::string whereOperator = "AND";
-		if (boost::contains(boost::to_lower_copy(mainWhere), "and") ||
-			boost::contains(boost::to_lower_copy(mainWhere), " or ")) {
-			multipleWhere = true;
-			size_t found;
-
-			if (boost::contains(boost::to_lower_copy(mainWhere), " or ")) {
-				std::cout << "SE DETECTÓ UN OR" << std::endl;
-				found = boost::to_lower_copy(mainWhere).find(" or ");
-				whereOperator = "OR";
-			} else {
-				std::cout << "SE DETECTÓ UN AND" << std::endl;
-				found = boost::to_lower_copy(mainWhere).find(" and ");
-				whereOperator = "AND";
-			}
-
-
-			wheres.push_back(mainWhere.substr(0, found));
-			wheres.push_back(mainWhere.substr(found + 5));
-			std::cout << wheres.at(0) << std::endl;
-			std::cout << wheres.at(1) << std::endl;
-
-		} else {
-			wheres.push_back(mainWhere);
-		}
-
-		// En caso de AND, OR, se hace esto por cada Where de forma separada
-		for (auto &where : wheres) {
-
-			std::string whereTable;
-			std::string whereColumn;
-			std::string whereEquals;
-			std::string whereOperator = "=";
-
-			// WhereParts contiene (ELEMENTO0) = (ELEMENTO1) del WHERE, donde (ELEMENTO0) = TABLA.COLUMNA
-			std::vector<std::string> whereParts;
-			boost::split(whereParts, where, boost::is_any_of("=<>"));
-
-			if (boost::contains(where, "<")) {
-				whereOperator = "<";
-			} else if (boost::contains(where, ">")) {
-				whereOperator = ">";
-			}
-
-			for (auto &element : whereParts) {
-				boost::trim(element);
-			}
-
-			if (whereParts.size() != 2) {
-				std::cerr << "Error: WHERE statement invalid, related to '=', '<', '>' character" << std::endl;
-				throw std::exception();
-			}
-
-			whereEquals = whereParts.at(1);
-
-			// WhereParts contiene (ELEMENTO0).(ELEMENTO1) del selector del WHERE, o sea TABLA.COLUMNA
-			std::vector<std::string> whereSelectorParts;
-
-			boost::split(whereSelectorParts, whereParts.at(0), boost::is_any_of("."));
-
-			for (auto &element : whereSelectorParts) {
-				boost::trim(element);
-			}
-
-			if (whereSelectorParts.size() != 2) {
-				std::cerr << "Error: WHERE selector invalid" << std::endl;
-				throw std::exception();
-			}
-
-
-			whereTable = whereSelectorParts.at(0);
-			whereColumn = whereSelectorParts.at(1);
-
-			WhereSelector* ws = new WhereSelector();
-			ws->column = whereColumn;
-			ws->table = whereTable;
-			ws->opera = whereOperator;
-			ws->equals = whereEquals;
-			whereSelectors.push_back(*ws);
-		}
+		whereSelectors = getWheres(mainWhere, from);
 
 	}
-
 
 	// Importantes: whereSelectors, whereOperator, multipleWhere, hasWhere
 
@@ -264,16 +217,16 @@ void processSelect(std::string sql) {
 				joinExternalColumn = matchPart.substr(dotPosition + 1);
 				boost::trim(joinExternalColumn);
 			} else {
-					std::string checkInternal = matchPart.substr(0, dotPosition);
-					boost::trim(checkInternal);
+				std::string checkInternal = matchPart.substr(0, dotPosition);
+				boost::trim(checkInternal);
 
-					if (checkInternal != from) {
-						std::cerr << "Error: The internal table definition on the JOIN clause differs" << std::endl;
-						throw std::exception();
-					}
+				if (checkInternal != from) {
+					std::cerr << "Error: The internal table definition on the JOIN clause differs" << std::endl;
+					throw std::exception();
+				}
 
-					joinInternalColumn = matchPart.substr(dotPosition + 1);
-					boost::trim(joinInternalColumn);
+				joinInternalColumn = matchPart.substr(dotPosition + 1);
+				boost::trim(joinInternalColumn);
 			}
 
 
@@ -285,4 +238,191 @@ void processSelect(std::string sql) {
 	}
 
 
+}
+
+std::vector<WhereSelector> getWheres(std::string mainWhere, std::string defaultTable) {
+	std::vector<std::string> wheres;
+	std::string whereOperator = "AND";
+	std::vector<WhereSelector> whereSelectors;
+	bool multipleWhere = false;
+
+	if (boost::contains(boost::to_lower_copy(mainWhere), "and") ||
+		boost::contains(boost::to_lower_copy(mainWhere), " or ")) {
+		multipleWhere = true;
+		size_t found;
+
+		if (boost::contains(boost::to_lower_copy(mainWhere), " or ")) {
+			std::cout << "SE DETECTÓ UN OR" << std::endl;
+			found = boost::to_lower_copy(mainWhere).find(" or ");
+			whereOperator = "OR";
+		} else {
+			std::cout << "SE DETECTÓ UN AND" << std::endl;
+			found = boost::to_lower_copy(mainWhere).find(" and ");
+			whereOperator = "AND";
+		}
+
+
+		wheres.push_back(mainWhere.substr(0, found));
+		wheres.push_back(mainWhere.substr(found + 5));
+		std::cout << wheres.at(0) << std::endl;
+		std::cout << wheres.at(1) << std::endl;
+
+	} else {
+		wheres.push_back(mainWhere);
+	}
+
+	// En caso de AND, OR, se hace esto por cada Where de forma separada
+	for (auto &where : wheres) {
+
+		std::string whereTable;
+		std::string whereColumn;
+		std::string whereEquals;
+		std::string whereOperator = "=";
+
+		// WhereParts contiene (ELEMENTO0) = (ELEMENTO1) del WHERE, donde (ELEMENTO0) = TABLA.COLUMNA
+		std::vector<std::string> whereParts;
+		boost::split(whereParts, where, boost::is_any_of("=<>"));
+
+		if (boost::contains(where, "<")) {
+			whereOperator = "<";
+		} else if (boost::contains(where, ">")) {
+			whereOperator = ">";
+		}
+
+		for (auto &element : whereParts) {
+			boost::trim(element);
+		}
+
+		if (whereParts.size() != 2) {
+			std::cerr << "Error: WHERE statement invalid, related to '=', '<', '>' character" << std::endl;
+			throw std::exception();
+		}
+
+		whereEquals = whereParts.at(1);
+
+		// WhereParts contiene (ELEMENTO0).(ELEMENTO1) del selector del WHERE, o sea TABLA.COLUMNA
+		std::vector<std::string> whereSelectorParts;
+
+
+		if (boost::contains(whereParts.at(0), ".")) {
+			boost::split(whereSelectorParts, whereParts.at(0), boost::is_any_of("."));
+
+			for (auto &element : whereSelectorParts) {
+				boost::trim(element);
+			}
+
+			if (whereSelectorParts.size() != 2) {
+				std::cerr << "Error: WHERE selector invalid" << std::endl;
+				throw std::exception();
+			}
+
+
+			whereTable = whereSelectorParts.at(0);
+			whereColumn = whereSelectorParts.at(1);
+		} else {
+			whereColumn = whereParts.at(0);
+			whereTable = defaultTable;
+		}
+
+		WhereSelector *ws = new WhereSelector();
+		ws->column = whereColumn;
+		ws->table = whereTable;
+		ws->opera = whereOperator;
+		ws->equals = whereEquals;
+		whereSelectors.push_back(*ws);
+	}
+
+	return whereSelectors;
+}
+
+
+void processUpdate(std::string sql) {
+
+	// Obtiene la primera palabra que sería la tabla
+	std::string table = sql.substr(0, sql.find_first_of(" "));
+	std::string setStr;
+
+	// Busca entre SET (texto) WHERE en caso de que hay WHERE o final de linea
+	boost::regex exp("(?<= SET ).*?(?=(?: WHERE |$))", boost::regex_constants::icase);
+	boost::smatch match;
+
+
+	if (boost::regex_search(sql, match, exp)) {
+		setStr = match.str();
+		boost::trim(setStr);
+	} else {
+		std::cerr << "Error: SET statement invalid" << std::endl;
+		throw std::exception();
+	}
+
+
+	std::vector<UpdateDescriptor> sets;
+
+	if (boost::contains(setStr, ",")) {
+		std::cout << "MULTIPLES UPDATE" << std::endl;
+
+		std::vector<std::string> setStringVector;
+		boost::split(setStringVector, setStr, boost::is_any_of(","));
+
+		for (auto &element : setStringVector) {
+			boost::trim(element);
+
+			size_t equalPosition = element.find("=");
+
+			UpdateDescriptor *ud = new UpdateDescriptor();
+			ud->column = boost::trim_copy(element.substr(0, equalPosition));
+			ud->value = boost::trim_copy(element.substr(equalPosition));
+			sets.push_back(*ud);
+
+		}
+
+	} else {
+		std::cout << "UNICO SELECT" << std::endl;
+
+		size_t equalPosition = setStr.find("=");
+
+		UpdateDescriptor *ud = new UpdateDescriptor();
+		ud->column = boost::trim_copy(setStr.substr(0, equalPosition));
+		ud->value = boost::trim_copy(setStr.substr(equalPosition));
+		sets.push_back(*ud);
+	}
+
+
+	std::vector<WhereSelector> wheres;
+
+	if (boost::contains(boost::to_lower_copy(sql), " where ")) {
+		std::cout << "SE DETECTÓ UN WHERE" << std::endl;
+		std::string mainWhere;
+
+		// Busca entre el WHERE y el JOIN si lo hay (o final de linea)
+		boost::regex exp("(?<= WHERE ).*?(?=(?: SET |$))", boost::regex_constants::icase);
+		boost::smatch match;
+
+		std::cout << sql << std::endl;
+
+
+		if (boost::regex_search(sql, match, exp)) {
+			mainWhere = match.str();
+			boost::trim(mainWhere);
+		} else {
+			std::cerr << "Error: WHERE statement invalid" << std::endl;
+			throw std::exception();
+		}
+
+		wheres = getWheres(mainWhere, table);
+
+	}
+
+	std::cout << wheres.at(1).table << std::endl;
+
+}
+
+
+void processCreateTable(std::string sql) {
+	std::cout << "VOY A CREAR UNA TABLA" << std::endl;
+
+	std::string tableName = sql.substr(0, sql.find_first_of("("));
+	boost::trim(tableName);
+
+	std::cout << "TABLE NAME: " << tableName << std::endl;
 }
